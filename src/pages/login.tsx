96e105ca2622f2b0e1bf4c030db52a90fc5135bd
Nami <havenothorn@gmail.com>
2025-01-28 21:39:35 +0900

    feat: login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,36 @@
+import axios from "axios";
+import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
+
 export const Login = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    try {
+      const response = await axios.post(
+        "http://localhost:8000/cats/login",
+        data
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1>Login</h1>
-      <form>
-        <input type="text" placeholder="Username" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="email"
+          placeholder="Email"
+          {...register("email", { required: true })}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password", { required: true })}
+        />
+        <input type="submit" />
       </form>
     </div>
   );
